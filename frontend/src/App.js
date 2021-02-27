43115ce1ad9548f1e80c02d5ae2dcbf036e7f773
Abhishek <iitjeeabhishek@gmail.com>
2021-02-27 19:40:21 +0530

    Sharing code on github

---
+++ frontend/src/App.js
@@ -0,0 +1,65 @@
+import React from "react";
+import data from "./data";
+
+function App() {
+  return (
+    <div className="grid-container">
+      <header className="row">
+        <div>
+          <a className="brand" href="/">
+            Gangadri
+          </a>
+        </div>
+        <div>
+          <a href="/cart">Cart</a>
+          <a href="/signin">SignIn</a>
+        </div>
+      </header>
+
+      <main>
+        <div className="row center">
+          {data.products.map((product) => (
+            <div key={product._id} className="card">
+              <a href={`/product/${product._id}`}>
+                <img
+                  className="medium"
+                  src={product.image}
+                  // alt={product.name}
+                />
+              </a>
+              <div className="card-body">
+              <a href={`/product/${product._id}`}>
+                  <h2>{product.name}</h2>
+                </a>
+                <div className="rating">
+                  <span>
+                    <i className="fa fa-star"></i>
+                  </span>
+                  <span>
+                    <i className="fa fa-star"></i>
+                  </span>
+                  <span>
+                    <i className="fa fa-star"></i>
+                  </span>
+                  <span>
+                    <i className="fa fa-star"></i>
+                  </span>
+                  <span>
+                    <i className="fa fa-star"></i>
+                  </span>
+                </div>
+                <div className="price">
+                  <h4>Rs. {product.price}</h4>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
+
+      <footer className="row center">All rights reserved &reg;</footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/data.js
@@ -0,0 +1,105 @@
+const data = {
+    products: [
+        {
+            _id:'1',
+            name: 'Tommy Hilifiger T-Shirt',
+            category: 'Shirts',
+            image: './images/p1.jpg',
+            price: 1200,
+            brand: 'Tommy Hilifiger',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product'
+        },
+        {
+            _id:'2',
+            name: 'Puma Shirt',
+            category: 'Pants',
+            image: './images/p2.jpg',
+            price: 499,
+            brand: 'Puma',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product'
+        },
+        {
+            _id:'3',
+            name: 'Van Huesan Shirt',
+            category: 'Shirts',
+            image: './images/p3.jpg',
+            price: 299,
+            brand: 'Van-Huesan',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product'
+        },
+        {
+            _id:'4',
+            name: 'Adida Slim T-Shirt',
+            category: 'Shirts',
+            image: './images/p4.jpg',
+            price: 1200,
+            brand: 'Adidas',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product'
+        },
+        {
+            _id:'5',
+            name: 'Arrow Slim Shirt',
+            category: 'Shirts',
+            image: './images/p5.jpg',
+            price: 849,
+            brand: 'Arrow',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product'
+        },
+        {
+            _id:'6',
+            name: 'Levis T-Shirt',
+            category: 'Shirts',
+            image: './images/p6.jpg',
+            price: 1120,
+            brand: 'Levis',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product'
+        },
+        {
+            _id:'7',
+            name: 'Roadstar T-Shirt',
+            category: 'Shirts',
+            image: './images/p7.jpg',
+            price: 449,
+            brand: 'Roadstar',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product'
+        },
+        {
+            _id:'8',
+            name: 'Wrangler Slim T-Shirt',
+            category: 'Shirts',
+            image: './images/p8.jpg',
+            price: 1800,
+            brand: 'Wrangler',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product'
+        },
+        {
+            _id:'9',
+            name: 'Myntra T-Shirt',
+            category: 'Shirts',
+            image: 'Ecommerce/frontend/public/images/p4.jpeg',
+            price: 800,
+            brand: 'Myntra',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product'
+        },
+    ]
+}
+
+export default data;
